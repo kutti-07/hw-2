663196ae2b25d71c6c3d648af3a4ea5b9f0ef287
kutti-07 <134058590+kutti-07@users.noreply.github.com>
2024-08-05 16:03:20 +0300

    Add files via upload

---
+++ project/js/lesson.js
@@ -0,0 +1,13 @@
+// PHONE BLOCK 
+
+const phoneInput = document.querySelector('#phone_input')
+const phoneButton = document.querySelector('#phone_button')
+const phoneSpan = document.querySelector('#phone_span')
+
+const regExp = /\+996 [2579]\d{2}-\d{2}-\d{2}-\d{2}/
+
+phoneButton.onclick = () => {
+    if (regExp) {
+         
+    }
+}
